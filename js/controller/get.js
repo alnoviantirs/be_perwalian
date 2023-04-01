b41ada11d7485f69e8a421a9a814a4f262b779c0
Novi <alnoviantiramadhani91@gmail.com>
2023-04-01 15:43:10 +0700

    6-2B-1214032-Al Novianti Ramadhani Sulaksono

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,12 +1,12 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { iniTabel } from "../temp/table.js";
 export function isiTablePerwalian(results) {
     results.forEach(isiRow);
 }
 function isiRow(value) {
     let content =
-        isiTabel.replace("#NAMA#", value.biodata.nama)
+        iniTabel.replace("#NAMA#", value.biodata.nama)
             .replace("#NOHP#", value.biodata.phone_number)
             .replace("#JURUSAN#", value.biodata.jurusan);
     addInner("iniTabel", content);
